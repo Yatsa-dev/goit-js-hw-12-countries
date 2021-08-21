23c85bece72a9c542d9718dff56e6862bc58c597
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-08-21 11:42:50 +0300

    clear code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 'use stricts';
 
+import Notiflix from "notiflix";
 import debounce from 'lodash.debounce';
 import countryTemplates from './templates/country-card.hbs';
 import countryItemTpl from './templates/country-list.hbs'
-import Notiflix from "notiflix";
+import FetchAPI from './js/fetchCountries'
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 500;
@@ -16,14 +17,14 @@ refs.serchForm.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(event) {
     event.preventDefault();
-    const inputValue = refs.serchForm.value;
-    if (inputValue === '') {
+    const searchQuery = refs.serchForm.value;
+
+    if (searchQuery === '') {
         refs.countryList.innerHTML = ''
         refs.cardContainer.innerHTML = ''
         return
     }
-
-    fetchCountries(inputValue)
+    FetchAPI.fetchCountries(searchQuery)
         .then(response => {
             if (response.length === 1) {
                 renderCountryCard(response);
@@ -43,22 +44,21 @@ function onSearch(event) {
             if (response.length > 10) {
                 Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
             }
-        }).catch(error => {
-            if (error === 404) {
-                Notiflix.Notify.failure('Oops, there is no country with that name')
-            }
         })
+        .catch(onFetchError)
         .finally(
             refs.cardContainer.innerHTML = ''
         );
 }
-function fetchCountries(name) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
-        .then(response => (response.ok)
-            ? response.json()
-            : Promise.reject(response.status))
+function onFetchError(error) {
+    if (error === 404) {
+        Notiflix.Notify.failure('Oops, there is no country with that name')
+    }
 }
 function renderCountryCard(country) {
     const markup = countryTemplates(...country);
     refs.cardContainer.insertAdjacentHTML('beforeend', markup);
 }
+
+
+
